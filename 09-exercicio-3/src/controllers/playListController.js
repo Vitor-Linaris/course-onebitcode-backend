2d0adc5902baa7bcb9b20a733fecf75e55502b01
Vitor-Linaris <vitor.linaris@gmail.com>
2024-11-14 21:16:16 -0300

    feat: add exercice 09

--- 09-exercicio-3/src/controllers/playListController.js
+++ 09-exercicio-3/src/controllers/playListController.js
@@ -44,6 +44,35 @@ module.exports = {
   },
 
   // Put /playlists/:id
+  update: (req, res) => {
+    const { id } = req.params;
+    const { name, tags, musics } = req.body;
+
+    playlistIndex = playlists.findIndex((playlist) => playlist.id === +id);
+
+    if (playlistIndex === -1) {
+      return res.status(404).json({ message: "Playlist não encontrada" });
+    }
+
+    playlists[playlistIndex].name = name;
+    playlists[playlistIndex].tags = tags;
+    playlists[playlistIndex].musics = musics;
+
+    res.json(playlists[playlistIndex]);
+  },
 
   // Delete /playlists/:id
+  delete: (req, res) => {
+    const { id } = req.params;
+
+    playlistIndex = playlists.findIndex((playlist) => playlist.id === +id);
+
+    if (playlistIndex === -1) {
+      return res.status(404).json({ message: "Playlist não encontrada" });
+    }
+
+    playlists.splice(playlistIndex, 1);
+
+    res.status(204).end();
+  },
 };
--- 09-exercicio-3/src/routes.js
+++ 09-exercicio-3/src/routes.js
@@ -6,5 +6,7 @@ const router = express.Router();
 router.get("/playlists", playListController.index);
 router.get("/playlist/:id", playListController.show);
 router.post("/playlist", playListController.save);
+router.put("/playlist/:id", playListController.update);
+router.delete("/playlist/:id", playListController.delete);
 
 module.exports = router;
